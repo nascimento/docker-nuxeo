3e558d1f8f35e0af1eff7e464bb1878e0c710049
Vitor Nascimento <vitor.nascimento@arizona.global>
2016-08-11 14:32:07 -0300

    Nuxeo 8.3 com S3

--- nuxeo/arizona_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Install packages if exist
-if [ -n "$NUXEO_ARIZONA_PACKAGES" ]; then
-  gosu $NUXEO_USER nuxeoctl mp-install $NUXEO_ARIZONA_PACKAGES --relax=false --accept=true --quiet
-fi
