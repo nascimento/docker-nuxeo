61a9872ecaeef2505b6eef01bf833c03dc8d9c81
Vitor Nascimento <vitor.nascimento@arizona.global>
2016-08-18 09:27:53 -0300

    Mysql

--- nuxeo/local_packages.sh
+++ nuxeo/local_packages.sh
@@ -2,5 +2,9 @@
 
 # Install packages if exist
 if [ -n "$LOCAL_PACKAGES" ]; then
-  gosu $NUXEO_USER nuxeoctl mp-install $LOCAL_PACKAGES
+  for pack in $LOCAL_PACKAGES;
+  do
+    gosu $NUXEO_USER nuxeoctl mp-remove $(basename $pack .zip) --accept yes --quiet
+  done
+  gosu $NUXEO_USER nuxeoctl mp-install $LOCAL_PACKAGES --accept yes --quiet
 fi
